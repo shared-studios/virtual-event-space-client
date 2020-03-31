0fb10d548ea8e5b9b7126f1a12997c7096036a99
Siam Mridha <siam@sharedstudios.com>
2020-03-31 18:23:40 -0400

    created register page mokup

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 const Header = () => {
     return <div className={styles.header}>
         <div className={styles.logo}>CONFERENCE 2020</div>
-        <Link className={styles.link} to='/programs'>PROGRAM</Link>
+        <Link className={styles.link} to='/programs'>PROGRAMS</Link>
         <Link className={styles.link} to='/KeynoteSpeakers'>KEYNOTE SPEAKERS</Link>
         <Link className={styles.link} to='/BreakoutSessions'>BREAKOUT SESSIONS</Link>
         <Link className={styles.link} to='/register'>REGISTER</Link>
--- src/components/register/index.js
+++ src/components/register/index.js
@@ -2,7 +2,18 @@ import React from 'react'
 import styles from './styles.module.css'
 
 const Register = () => {
-    return <div className={styles.register}>Register Page</div>
+    return (
+        <div className={styles.register}>
+            <div className={styles.avatar} />
+            <input name='name' className={`${styles.name_input} ${styles.register_input}`} placeholder="Name" />
+            <input name='company' className={`${styles.company_input} ${styles.register_input}`} placeholder="Company/Organization" />
+            <div className={`${styles.event_input} ${styles.register_input}`}>
+                Please select at list one scheduled events on the left you'd like to attend
+            </div>
+            <button className={styles.create_badge_button}>Create badge</button>
+            <button className={styles.cancel_button}>Cancel</button>
+        </div>
+    )
 }
 
-export default Register
+export default React.memo(Register)
